28d694fcc7232e28c8a5212c51ad46045b621d42
carolineanneok <carolineanneok@github.com>
2019-03-22 01:15:36 -0700

    trying to change cursor

--- style.js
+++ style.js
@@ -1,1 +1,12 @@
 
+var cont = document.getElementbyId('container');
+cont.onmousemove=applyCustomPointer;
+
+var customPointer = document.getElementbyId('customPointer');
+
+function applyCustomPointer(event) {
+  customPointer.style.left= event.clientx - (customPointer.offsetWidth / 2) + 'px';
+  customPointer.style.top = event.clienty - (customPointer.offsetHeight / 2) + 'px';
+ 
+
+}
