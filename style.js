23df755fcef0b8a8d6beb65fe944ba0bbaef22fa
carolineanneok <carolineanneok@github.com>
2019-03-21 23:32:14 -0700

    started working on facebook click tally function

--- style.js
+++ style.js
@@ -1,1 +1,14 @@
+'use strict';
+var clickCounter = 0;
+var clicks = document.createElement('button');
 
+var click = allimg[i].clicks;
+
+var clickArray = [];
+
+function facebookClicks() {
+    for (var i = 0; i < allimg.length; i++) {
+      clickArray.push(allimg[i].clicks);
+    }
+
+    
