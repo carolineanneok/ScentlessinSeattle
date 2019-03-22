b83bc03be172c4309ae134b96f4f46a313a3f1a2
carolineanneok <carolineanneok@github.com>
2019-03-22 11:35:45 -0700

    trying to fix conflict in js

--- style.js
+++ style.js
@@ -10,3 +10,14 @@ function applyCustomPointer(event) {
  
 
 }
+
+
+var click = allbutton[i].clicks;
+
+   var clickArray = [];
+
+function facebookClicks() {
+    for (var i = 0; i < allbutton.length; i++) {
+      clickArray.push(allbutton[i].clicks);
+    }
+}
