b67fe78f3a410bb4d79d13ac3e017a82b8b16ef9
carolineanneok <carolineanneok@github.com>
2019-03-22 18:49:20 -0700

    added alert for blog page

--- style.js
+++ style.js
@@ -1,14 +1,18 @@
 'use strict';
-var clickCounter = 0;
-var clicks = document.createElement('button');
 
-var click = allbutton[i].clicks;
+window.alert('This page is still under construction. Stay Tuned');
 
-   var clickArray = [];
+//
+//var clickCounter = 0;
+//var clicks = document.createElement('button');
 
-function facebookClicks() {
-    for (var i = 0; i < allbutton.length; i++) {
-      clickArray.push(allbutton[i].clicks);
-    }
-}
+//var click = allbutton[i].clicks;
+
+   /////var clickArray = [];
+
+//function facebookClicks() {
+    //for (var i = 0; i < allbutton.length; i++) {
+      //clickArray.push(allbutton[i].clicks);
+   // }
+
      
