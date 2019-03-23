9470da6b19c31537c63dc8b9b6856f191d9aebc8
carolineanneok <carolineanneok@github.com>
2019-03-23 15:37:21 -0700

    fixed js links in html

--- style.js
+++ style.js
@@ -1,18 +1,27 @@
 'use strict';
 
-window.alert('This page is still under construction. Stay Tuned');
 
-//
-//var clickCounter = 0;
-//var clicks = document.createElement('button');
+var clickCounter = 0;
+var clicks = document.createElement('button');
 
-//var click = allbutton[i].clicks;
+var click = allbutton[i].clicks;
 
-   /////var clickArray = [];
+var clickArray = [];
 
-//function facebookClicks() {
-    //for (var i = 0; i < allbutton.length; i++) {
-      //clickArray.push(allbutton[i].clicks);
-   // }
+function facebookClicks() {
+    for (var i = 0; i < allbutton.length; i++) {
+      clickArray.push(allbutton[i].clicks);
+   }
 
-     
+   //Save the facebook button click counts to local storage.
+   //when the page reloads it will show previous counts
+   //save the local storage code in the click counter function
+document.getElementById("button").addEventListener("mouseover", facebookClicks);{
+}
+count=localStorage.getItem('facebookClicks');{
+}
+}
+
+document.getElementById("button").addEventListener("click", function(){
+   this.style.backgroundColor = "blue";
+ });
